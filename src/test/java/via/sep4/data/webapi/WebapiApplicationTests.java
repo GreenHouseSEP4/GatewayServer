1a6781d70b0a6d81a440a962f5d306a0d54fb7cc
MariAsenova <maria@admill.dk>
2021-11-13 15:40:23 +0100

    initial spring boot RESTfull API

---
+++ src/test/java/via/sep4/data/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sep4.data.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
