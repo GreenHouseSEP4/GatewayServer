f7d5fd4aa8b1726eec3aab67a30cf0b0090e4187
MariAsenova <64922954+MariAsenova@users.noreply.github.com>
2021-12-16 21:19:51 +0100

    hadle exception in measureemnts

--- src/main/java/via/sep4/data/webapi/service/measurement/MeasurementServiceImpl.java
+++ src/main/java/via/sep4/data/webapi/service/measurement/MeasurementServiceImpl.java
@@ -2,7 +2,10 @@ package via.sep4.data.webapi.service.measurement;
 
 import java.util.*;
 
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.stereotype.Service;
 
 import via.sep4.data.webapi.model.SensorData;
@@ -16,34 +19,33 @@ public class MeasurementServiceImpl implements MeasurementService {
     private MeasurementRepository measurementRepository;
 
     @Override
-    public SensorData addMeasurement(SensorData data) {
+    public SensorData addMeasurement(SensorData data) throws NotFoundException {
         try {
             return measurementRepository.save(data);
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Sensor repository not available.");
+            throw new NotFoundException("Data could not be saved");
         }
     }
 
     @Override
-    public SensorData getLatestMeasurement(String eui) {
+    public SensorData getLatestMeasurement(String eui) throws NotFoundException {
         try {
             return measurementRepository.findFirstByEuiOrderByIdDesc(eui);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Sensor repository not available.");
+            throw new NotFoundException("Data could not be found ");
         }
     }
 
     @Override
-    public List<SensorData> getPeriodicMeasurements(String eui, Date startDate, Date endDate) {
+    public List<SensorData> getPeriodicMeasurements(String eui, Date startDate, Date endDate) throws NotFoundException {
         try {
             List<SensorData> all = measurementRepository.findByEuiAndDateBetween(eui, startDate, endDate);
             Collections.sort(all, new SortByDate());
             return all;
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("The dates are out of bounds.");
+            throw new NotFoundException("Data could not be found");
         }
     }
 }
